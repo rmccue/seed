3790e4e02cfd22dda20d6c1a2267279449bab7fd
Ryan McCue <ryanmccue@cubegames.net>
2009-09-27 11:17:39 +1000

    Quick rough draft

---
+++ ideas.php
@@ -0,0 +1,17 @@
+<?php
+
+$topics = array(
+	'test-idea' => array(
+		'title' => 'Test Idea',
+		'votes' => 0
+	);
+);
+
+foreach($topics as $topic) {
+?>
+<div class="topic">
+	<h2 class="title"><?php echo $topic['title'] ?></h2>
+	<p class="votes"><?php echo $topic['votes'] ?></p>
+</div>
+<?php
+}
