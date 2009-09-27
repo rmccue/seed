8f2207af9f6427d9b19b12c7deb3454886a68443
Ryan McCue <ryanmccue@cubegames.net>
2009-09-27 11:37:21 +1000

    Add rudimentary voting

--- ideas.php
+++ ideas.php
@@ -10,6 +10,7 @@ $topics = array(
 <!doctype html>
 <html>
 <head>
+	<title>Ideas</title>
 	<style>
 	@import url("reset.css");
 	
@@ -31,17 +32,27 @@ $topics = array(
 		margin: 5px 20px 0;
 	}
 	</style>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 </head>
 <body>
 <?php
 foreach($topics as $topic) {
 ?>
-<div class="topic">
-	<h2 class="title"><?php echo $topic['title'] ?></h2>
-	<p class="votes"><?php echo $topic['votes'] ?></p>
-</div>
+	<div class="topic">
+		<h2 class="title"><?php echo $topic['title'] ?></h2>
+		<p class="votes"><?php echo $topic['votes'] ?></p>
+	</div>
 <?php
 }
 ?>
+	<script>
+		$(document).ready(function () {
+			$(".votes").css("cursor", "pointer").click(function () {
+				old = $(this).text();
+				$(this).text(++old);
+				return false;
+			});
+		});
+	</script>
 </body>
 </html>
