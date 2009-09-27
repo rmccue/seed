37420448de236564926a54aab7db440dd5faf74d
Ryan McCue <ryanmccue@cubegames.net>
2009-09-27 20:51:13 +1000

    Add configuration file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$topics = array(
+	'test-idea' => array(
+		'title' => 'Test Idea',
+		'votes' => 1
+	),
+	'test-idea2' => array(
+		'title' => 'Test Idea 2',
+		'votes' => 2
+	),
+	'test-idea3' => array(
+		'title' => 'Test Idea 3',
+		'votes' => 3
+	),
+);
